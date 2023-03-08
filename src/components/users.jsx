6c07175114933dc91552e7c74aa958263b4eb69c
Alexey Danilchik <butuzz74@gmail.com>
2023-03-08 18:24:17 +0300

    work on mistakes

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,14 +1,38 @@
 import React, { useState } from "react";
 import API from "../api";
-import User from "./user";
-import Header from "./header";
-import TableHead from "./tableHead";
 
 const Users = () => {
   const [usersData, setUsers] = useState(API.users.fetchAll());
 
-  const handleDelete = (event) => {
-    setUsers(usersData.filter((el) => el._id !== event.target.id));
+  const handleDelete = (id) => {
+    setUsers(usersData.filter((el) => el._id !== id));
+  };
+
+  const User = (user, handleDelete) => {
+    return (
+      <tr key={user._id}>
+        <td>{user.name}</td>
+        <td>
+          {user.qualities.map((el) => (
+            <span key={el._id} className={`badge bg-${el.color} ms-3`}>
+              {el.name}
+            </span>
+          ))}
+        </td>
+        <td>{user.profession.name}</td>
+        <td>{user.completedMeetings}</td>
+        <td>{user.rate}/5</td>
+        <td>
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={() => handleDelete(user._id)}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
   };
 
   return (
@@ -19,10 +43,27 @@ const Users = () => {
         </h1>
       ) : (
         <>
-          <>{Header(usersData)}</>
+          {usersData.length > 4 || usersData.length === 1 ? (
+            <h1 className="badge bg-primary fs-4">
+              {`${usersData.length} человек тусанет с тобой сегодня`}
+            </h1>
+          ) : (
+            <h1 className="badge bg-primary fs-4">
+              {`${usersData.length} человека тусанет с тобой сегодня`}
+            </h1>
+          )}
           <br />
           <table className="table">
-            <TableHead />
+            <thead className="border-bottom-0 border-2">
+              <tr>
+                <th>Имя</th>
+                <th>Качества</th>
+                <th>Профессия</th>
+                <th>Встретился,раз</th>
+                <th>Оценка</th>
+                <th></th>
+              </tr>
+            </thead>
             <tbody>{usersData.map((user) => User(user, handleDelete))}</tbody>
           </table>
         </>
